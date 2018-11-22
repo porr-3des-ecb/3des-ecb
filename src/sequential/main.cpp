736bfc47eb1276afa8f99f757c077dc371b86b80
mjbogusz <mjbogusz+github@gmail.com>
2018-11-22 02:15:28 +0100

    initial CMake files and empty main file for sequential version

---
+++ src/sequential/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int tdesSequential(std::istream input, std::ostream output, std::string key) {
+	/// TODO
+}
+
+int main(int argc, char const *argv[]) {
+	if (argc != 4) {
+		std::cout << "Usage: " << argv[0] << " <inputFile> <outputFile> <key>" << std::endl;
+		return 1;
+	}
+
+	std::ifstream iFile(argv[1]);
+	if (!iFile.good()) {
+		std::cout << "Opening file " << argv[1] << " for reading failed" << std::endl;
+		return 2;
+	}
+
+	std::ofstream oFile(argv[2]);
+	if (!oFile.good()) {
+		std::cout << "Opening file " << argv[2] << " for writing failed" << std::endl;
+		return 2;
+	}
+
+	std::string key = argv[3];
+
+	/// TODO: start time measurements
+
+	tdesSequential(iFile, oFile, key);
+
+	/// TODO: end and display time measurements
+
+	iFile.close();
+	oFile.close();
+
+	return 0;
+}
